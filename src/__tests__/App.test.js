d5d62ce0c5d4412504cc0537baf9a0de473a6168
Karthikeyan21001828 <karthi06062004@gmail.com>
2025-04-10 16:16:10 +0530

    test file updated

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('MCQ App', () => {
   test('renders the first question on load', () => {
